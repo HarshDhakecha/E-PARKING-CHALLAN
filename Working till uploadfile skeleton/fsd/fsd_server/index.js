eb907ad5c5e8a7ebf18b22a172d3eb293b858a31
OmOslaniya <omoslaniya92@gmail.com>
2024-02-18 16:51:45 +0530

    .env changed

--- Working till uploadfile skeleton/fsd/fsd_server/index.js
+++ Working till uploadfile skeleton/fsd/fsd_server/index.js
@@ -21,7 +21,7 @@ app.listen(portnumber, () => {
 });
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", `${process.env.REACT_APP_API_URL}`);
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
