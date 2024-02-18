ccc6b643584602817f753bb71d965ae71bbfe39e
OmOslaniya <omoslaniya92@gmail.com>
2024-02-18 21:02:59 +0530

    updated python server3

--- Working till uploadfile skeleton/fsd/fsd_client/src/pages/getimage.py
+++ Working till uploadfile skeleton/fsd/fsd_client/src/pages/getimage.py
@@ -8,7 +8,7 @@ import re
 from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://main.dwdjviht0eipb.amplifyapp.com/officerhome"}})
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/upload', methods=['POST'])
 
