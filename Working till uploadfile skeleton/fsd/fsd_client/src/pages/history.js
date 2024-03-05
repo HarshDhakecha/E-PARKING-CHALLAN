e1d211a2dc199f328fec158f78d043252472d30e
OmOslaniya <omoslaniya92@gmail.com>
2024-03-05 16:19:57 +0530

    notify

--- Working till uploadfile skeleton/fsd/fsd_client/src/pages/history.js
+++ Working till uploadfile skeleton/fsd/fsd_client/src/pages/history.js
@@ -4,23 +4,35 @@ import { luser } from './Signin';
 import Navbar2 from './navbar2';
 import MainNavbar from './mainnavbar';
 import Footer from './footer';
-import './history.css'; 
+import './history.css';
 
 const History = () => {
   const [detailsData, setDetailsData] = useState([]);
-  const [expandedIndices, setExpandedIndices] = useState([]);
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [showUnpaidMemos, setShowUnpaidMemos] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedYear, setSelectedYear] = useState('');
+  const [selectedMonth, setSelectedMonth] = useState('');
 
 
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log("luser is "+ luser);
-        const res = await fetch(`${process.env.REACT_APP_API_URL}/fetchhistory`, {
+        let apiUrl = '/fetchAllMemos';
+
+        // If year and month are selected, use the new endpoint
+        if (selectedYear && selectedMonth) {
+          apiUrl = '/fetchMemosByYearMonth';
+        }
+
+        const res = await fetch(`${process.env.REACT_APP_API_URL}${apiUrl}`, {
           method: 'POST',
           headers: { 'Content-type': 'application/json' },
           body: JSON.stringify({
             luser,
+            status: showUnpaidMemos ? 'false' : 'true',
+            year: selectedYear,
+            month: selectedMonth,
           }),
         });
 
@@ -32,27 +44,102 @@ const History = () => {
     };
 
     fetchData();
-  }, [luser]);
+  }, [luser, showUnpaidMemos, selectedYear, selectedMonth]);
+
 
   const handleToggleDetails = (index) => {
-    setExpandedIndices((prevIndices) => {
-      if (prevIndices.includes(index)) {
-        // Remove index if already expanded
-        return prevIndices.filter((i) => i !== index);
-      } else {
-        // Add index if not expanded
-        return [...prevIndices, index];
-      }
-    });
+    setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
-  return (
-    <div>
-      <MainNavbar />
-      <Navbar2 />
 
-      <div className="container mt-3">
-        {/* <h3 className="text-center mb-4">Memo History for {luser}</h3> */}
+  const handleYearChange = (event) => {
+    const selectedYear = event.target.value;
+    setSelectedYear(selectedYear);
+
+    // Clear the selected month when the year changes
+    setSelectedMonth('');
+  };
+
+  const handleMonthChange = (event) => {
+    setSelectedMonth(event.target.value);
+  };
+
+
+  const handleNotifyAll = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/notifyAll`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          luser,
+          memos: detailsData.filter((obj) => obj.flag === 'false').map((obj) => obj.memo_number),
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Notify emails sent successfully.');
+        // You can add additional logic or feedback to the user if needed.
+      } else {
+        console.error('Error sending notification emails.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const renderAdditionalDetails = (obj, index) => {
+    if (expandedIndex === index) {
+      return (
+        <div className="additional-details">
+          <div className="table-responsive">
+            <table className="table table-bordered table-sm">
+              <tbody>
+                <tr>
+                  <th scope="row">Memo Number</th>
+                  <td>{obj.memo_number}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Owner Name</th>
+                  <td>{obj.pobj.name}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Address</th>
+                  <td>{obj.pobj.address}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Vehicle Type</th>
+                  <td>{obj.pobj.vehicle_type}</td>
+                </tr>
+                {obj.flag === 'true' && (
+                  <tr>
+                    <th scope="row">Pay Date</th>
+                    <td>{new Date(obj.status).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderTable = () => {
+      const filteredData = Array.isArray(detailsData) ? detailsData.filter(obj =>
+        obj.pobj.no_plate.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        obj.memo_number.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        obj.pobj.name.toLowerCase().includes(searchTerm.toLowerCase())
+      ) : [];
+    
+
+    if (showUnpaidMemos) {
+      const unpaidMemos = filteredData.filter((obj) => obj.flag === 'false');
+
+      return (
+        <div>
         <table className="table">
           <thead>
             <tr>
@@ -64,34 +151,161 @@ const History = () => {
             </tr>
           </thead>
           <tbody>
-            {detailsData.map((obj, index) => (
-              <tr key={index}>
-                <th scope="row">{index + 1}</th>
-                <td>{obj.pobj.no_plate}</td>
-                <td>{obj.date}</td>
-                <td className={obj.flag === 'true' ? 'text-success' : 'text-danger'}>
-                  {obj.flag === 'true' ? 'Paid' : 'Unpaid'}
-                </td>
-                <td>
-                  <button
-                    className={`btn btn-link toggle-button ${expandedIndices.includes(index) ? 'expanded' : ''}`}
-                    onClick={() => handleToggleDetails(index)}
-                  >
-                    {expandedIndices.includes(index) ? 'Less' : 'More'}
-                  </button>
-                </td>
-              </tr>
+            {unpaidMemos.map((obj, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{obj.pobj.no_plate}</td>
+                  <td>{new Date(obj.date).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</td>
+                  <td className={obj.flag === 'true' ? 'text-success' : 'text-danger'}>
+                    {obj.flag === 'true' ? 'Paid' : 'Unpaid'}
+                  </td>
+                  <td>
+                    <button
+                      className={`btn btn-link toggle-button ${expandedIndex === index ? 'expanded' : ''}`}
+                      onClick={() => handleToggleDetails(index)}
+                    >
+                      {expandedIndex === index ? (
+                        <i className="bi bi-dash"></i>
+                      ) : (
+                        <i className="bi bi-plus"></i>
+                      )}
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="5">{renderAdditionalDetails(obj, index)}</td>
+                </tr>
+              </React.Fragment>
             ))}
           </tbody>
         </table>
-        {expandedIndices.map((index) => (
-          <div key={index} className="card mt-3 custom-card">
-            <div className="card-body">
-              {/* Additional details for the expanded row */}
-              {/* ... */}
-            </div>
+
+        {unpaidMemos.length > 0 && (
+        <div className="text-end">
+          <button className="btn btn-primary" onClick={handleNotifyAll}>
+            Notify All
+          </button>
+        </div>
+      )}
+      
+        </div>
+      );
+    } else {
+      // Render the complete table
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">No Plate</th>
+              <th scope="col">Date</th>
+              <th scope="col">Status</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((obj, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{obj.pobj.no_plate}</td>
+                  <td>{new Date(obj.date).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</td>
+                  <td className={obj.flag === 'true' ? 'text-success' : 'text-danger'}>
+                    {obj.flag === 'true' ? 'Paid' : 'Unpaid'}
+                  </td>
+                  <td>
+                    <button
+                      className={`btn btn-link toggle-button ${expandedIndex === index ? 'expanded' : ''}`}
+                      onClick={() => handleToggleDetails(index)}
+                    >
+                      {expandedIndex === index ? (
+                        <i className="bi bi-dash"></i>
+                      ) : (
+                        <i className="bi bi-plus"></i>
+                      )}
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="5">{renderAdditionalDetails(obj, index)}</td>
+                </tr>
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <MainNavbar />
+      <Navbar2 />
+
+      <div className="container mt-3">
+        <div className="mb-3 d-flex justify-content-between align-items-center">
+          <div>
+            <button
+              className="btn btn-info"
+              onClick={() => setShowUnpaidMemos(!showUnpaidMemos)}
+            >
+              Unpaid Memos
+            </button>
           </div>
-        ))}
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button
+              className="btn btn-primary"
+              // onClick={handleSearch}
+            >
+              Search
+            </button>
+
+             {/* Year Dropdown */}
+             <select
+              className="form-select"
+              value={selectedYear}
+              onChange={handleYearChange}
+            >
+              <option value="">Select Year</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+
+            </select>
+
+            {/* Month Dropdown */}
+            <select
+              className="form-select"
+              value={selectedMonth}
+              onChange={handleMonthChange}
+              disabled={!selectedYear}
+            >
+             <option value="">Select Month</option>
+            <option value="01">January</option>
+  <option value="02">February</option>
+  <option value="03">March</option>
+  <option value="04">April</option>
+  <option value="05">May</option>
+  <option value="06">June</option>
+  <option value="07">July</option>
+  <option value="08">August</option>
+  <option value="09">September</option>
+  <option value="10">October</option>
+  <option value="11">November</option>
+  <option value="12">December</option>
+            </select>
+
+          </div>
+        </div>
+        {renderTable()}
       </div>
 
       <Footer />
--- Working till uploadfile skeleton/fsd/fsd_server/Routes/fetch_history.js
+++ Working till uploadfile skeleton/fsd/fsd_server/Routes/fetch_history.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./Login');
-const cors = require('cors')
+const cors = require('cors');
 const personSchema = require('../models/pdetails');
 const mongoose = require('mongoose');
 const Person = mongoose.model("person_detail", personSchema);
@@ -9,21 +9,19 @@ const authoritySchema = require("../models/authoritySchema");
 const Professor = mongoose.model("authority", authoritySchema);
 const memohistory = require("../models/memo_history");
 
-
-
 const app = express();
 const port = 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-router.post('/fetchhistory', async(req, res) => {
+
+router.post('/fetchAllMemos', async(req, res) => {
     try {
 
         let details = [];
         console.log("wwwwwwww");
         const { luser } = req.body;
         console.log(luser);
-        // console.log("welcome");
         const p = await Professor.findOne({ username: luser });
         // console.log(p);
         const police_id = p._id;
@@ -50,19 +48,69 @@ router.post('/fetchhistory', async(req, res) => {
             details.push(obj);
 
         }
-        // console.log(details);
+
+        res.json(details);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/fetchMemosByYearMonth', async(req, res) => {
+    try {
+
+        let details = [];
+        const { luser, year, month } = req.body;
+        console.log(year);
+        console.log(month);
+        const professor = await Professor.findOne({ username: luser });
+        console.log("active");
+        if (!professor) {
+            return res.status(404).json({ error: 'Professor not found' });
+        }
+
+        const police_id = professor._id;
+        console.log("aaa");
+
+        const memosByYearMonth = await memohistory.find({
+            authority: police_id,
+        });
+
+        console.log("All Memos:", memosByYearMonth);
+
+        const filteredMemos = memosByYearMonth.filter(memo => {
+            const memoDate = new Date(memo.memodate);
+            const memoYear = memoDate.getFullYear();
+            const memoMonth = memoDate.getMonth() + 1; // Months are zero-based
+
+            console.log("Memo Date, Year, Month:", memoDate, memoYear, memoMonth);
+
+            return memoYear === parseInt(year) && memoMonth === parseInt(month);
+        });
+
+        console.log("Filtered Memos:", filteredMemos);
+
+        for (let memo of filteredMemos) {
+            let pid = memo.person;
+            let date = memo.memodate;
+            let status = memo.paydate;
+            let flag = memo.flag;
+            let memo_number = memo.memo_number;
+
+            const pdetail = await Person.findOne({ _id: pid });
+
+            let obj = {
+                pobj: pdetail,
+                date: date,
+                status: status,
+                flag: flag,
+                memo_number: memo_number,
+            };
 
 
-        // const personIds = police1.map(entry => entry.person);
-        // const dates = police1.map(entry => entry.memodate);
-        // // console.log(personIds);
-
-
-        // const persons = await Person.find({ _id: { $in: personIds } });
-        // console.log(persons);
-        // console.log(dates);
-
-        // console.log(result.persons_data);
+            details.push(obj);
+        }
+        console.log(details);
         res.json(details);
     } catch (error) {
         res.status(500).json({ error: error.message });
